7ee1afcd604e8edc1c4a86b180a6774d7cef98b8
luciano-castillo <luchocastillo086@gmail.com>
2022-08-24 16:52:57 -0300

    ej5lab2

---
+++ labEj5/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        float n6=6;
+        float n5 = 5;
+        float n2 = 2 ;
+        float n3 = 3;
+        float n7 = 7;
+        float n30 =30;
+        float n1 = 1;
+
+        float numerador = n6 / n5 - n2/n3 * n7 / n2 + n2/n30;
+
+        float denomindador = n1/n3 /5;
+
+        float resultado = numerador / denomindador;
+
+        System.out.println(resultado);
+
+
+    }
+
+}
