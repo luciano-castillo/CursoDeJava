ef5ee21b3f343706d75eb9b725cd293f276e0035
luciano-castillo <luchocastillo086@gmail.com>
2022-08-24 17:22:25 -0300

    lab2 EJ 6

---
+++ lab2Ej6/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double numero=10.60;
+
+        System.out.println("El seno de "+numero+" es :"+Math.sin(numero));
+
+        System.out.println("El coseno de "+numero+" es :"+Math.cos(numero));
+
+        System.out.println("La tangente de "+numero+" es :"+Math.tan(numero));
+
+        double numero2 = 5.90;
+
+        System.out.println("El atan de "+numero2+" es : "+Math.atan(numero2));
+
+        System.out.println("El atan2 de "+numero+" y "+numero2+" es "+Math.atan2(numero,numero2));
+
+        System.out.println("El exponecinal de "+numero2+" es :"+Math.exp(numero2));
+
+        System.out.println("El logaritmo de "+numero+" es :"+Math.log(numero));
+
+        System.out.println("El numero PI es:"+Math.PI);
+
+        System.out.println("El numero E es :"+Math.E);
+
+    }
+}
