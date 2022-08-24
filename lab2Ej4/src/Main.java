5ad87513a0e9b34c82a9e7d39af8a36ba4cb98f7
luciano-castillo <luchocastillo086@gmail.com>
2022-08-24 15:19:56 -0300

    actividad4

---
+++ lab2Ej4/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        String nombre = "luciano";
+
+        int edad = 20;
+
+        double salario = 20000.55;
+
+        boolean carnet = true ;
+
+        System.out.println("Mi nombre es: "+nombre);
+
+        System.out.println("Tengo: "+edad);
+
+        System.out.println("Me gustaria ganar: "+salario);
+
+        System.out.println("Carnet: "+carnet);
+
+    }
+}
